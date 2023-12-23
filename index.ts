4e8c277feb3ce1abed7df3bcd815db1895a0871d
LaupWing <loc_strict@hotmail.com>
2023-12-23 18:41:27 +0100

    ahaa

--- index.ts
+++ index.ts
@@ -2,12 +2,34 @@ import winston from "winston"
 
 const logger = winston.createLogger({
    level: "info",
-   format: winston.format.json(),
+   format: winston.format.combine(
+      // winston.format.colorize({ all: true }),
+      winston.format.timestamp({
+         format: "YYYY-MM-DD HH:mm:ss"
+      }),
+      winston.format.align(),
+      winston.format.printf((info) => {
+         console.log(info.message)
+         console.log(typeof info.message)
+         return `[${info.timestamp}] ${info.level}: ${typeof info.message === "object" ? JSON.stringify(info.message, null, 2) : info.message}`
+      })
+   ),
    transports: [
-      new winston.transports.Console({
-         format: winston.format.simple()
+      new winston.transports.Console(),
+      new winston.transports.File({
+         filename: "logs/info.log",
+         format: winston.format.combine(
+            winston.format.json()
+         )
       })
    ]
 })
 
 logger.info("Hello world!")
+logger.error("Hello world!")
+console.log(typeof {
+   test: "Hello world!"
+})
+logger.info({
+   test: "Hello world!"
+})
