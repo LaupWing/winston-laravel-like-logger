3b77fc7342abf4585c6986387cfa8bd805253160
LaupWing <loc_strict@hotmail.com>
2023-12-23 17:54:07 +0100

    udpated

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("test")
+import winston from "winston"
+
+const logger = winston.createLogger({
+   level: "info",
+   format: winston.format.json(),
+   transports: [
+      new winston.transports.Console({
+         format: winston.format.simple()
+      })
+   ]
+})
+
+logger.info("Hello world!")
